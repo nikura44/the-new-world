5661a96916a8cdbfa7f022421170d245834bda48
Nikuras4444 <68015316@qq.com>
2021-11-01 17:23:46 +0800

    add big-screen component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="box">
+    <div class="left"><ContentSlot>nian</ContentSlot></div>
   </div>
 </template>
 
 <script>
+import ContentSlot from "../components/content-slot.vue";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: {
+    ContentSlot,
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  background-color: #000214;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.box .left {
+  width: 22.9%;
 }
 </style>
---
+++ src/components/content-slot.vue
@@ -0,0 +1,58 @@
+<template>
+<!-- 这是一个画盒子的组件 -->
+  <div class="box">
+    <div class="top-left"></div>
+    <div class="top-right"></div>
+    <div class="bottom-left"></div>
+    <div class="bottom-right"></div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss" scoped>
+.box {
+    width: 100%;
+    position: relative;
+    margin-bottom: 15px;
+}
+.box .top-right {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 15px;
+    height: 15px;
+    border-top: 2px solid #0033FA;
+    border-right: 2px solid #0033FA;
+}
+.box .top-left {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 15px;
+    height: 15px;
+    border-top: 2px solid #0033FA;
+    border-left: 2px solid #0033FA;
+}
+.box .bottom-left {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 15px;
+    height: 15px;
+    border-bottom: 2px solid #0033FA;
+    border-left: 2px solid #0033FA;
+}
+.box .bottom-right {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 15px;
+    height: 15px;
+    border-bottom: 2px solid #0033FA;
+    border-right: 2px solid #0033FA;
+}
+</style>
